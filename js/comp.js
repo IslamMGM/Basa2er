81797646d4a8223c1139375f5354e6a4702cd1d5
IslamMGM <eslamgamel111@gmail.com>
2024-08-28 12:50:45 +0300

    v4-12

--- js/comp.js
+++ js/comp.js
@@ -33,7 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".info").forEach(function (element) {
     element.addEventListener("click", function (e) {
       Swal.fire({
-        title: "إنتظر!",
         text: "سيتم توفير ملفات التدبر لكل مسار قريباً إن شاء الله",
         icon: "info",
         confirmButtonText: "حسناً",
