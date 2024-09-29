5b1f98327d57e19eb9df0648d2960cc125edf755
IslamMGM <eslamgamel111@gmail.com>
2024-09-29 11:02:26 +0300

    v2

---
+++ js/event-image.js
@@ -0,0 +1,28 @@
+let TrandingSlider = new Swiper(".img-event-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  initialSlide: 6,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 319,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 3,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".prev-btn",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
