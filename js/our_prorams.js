cc99a538a0edba97d334c850e1512ea86599d40c
IslamMGM <eslamgamel111@gmail.com>
2025-02-18 15:02:31 +0200

    program page v1

---
+++ js/our_prorams.js
@@ -0,0 +1,28 @@
+let TrandingSlider_party = new Swiper(".programs-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  initialSlide: 0,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 600,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 3,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".prev-btn",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
