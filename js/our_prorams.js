d36793b5bc83b9ed7617417b76fcdb3491d3e53f
IslamMGM <eslamgamel111@gmail.com>
2025-02-22 14:30:40 +0200

    program page v3

--- js/our_prorams.js
+++ js/our_prorams.js
@@ -22,7 +22,56 @@ let TrandingSlider_party = new Swiper(".programs-slider", {
     prevEl: ".prev-btn",
   },
   autoplay: {
-    delay: 3000,
+    delay: 8000,
     disableOnInteraction: false,
   },
+});
+
+///////////
+window.addEventListener("load", function () {
+  const slowContent = document.querySelector(".form-1");
+  const loader = document.querySelector(".loader");
+
+  slowContent.style.visibility = "visible"; // Show the content
+  loader.style.display = "none"; // Hide the loader
+});
+// ////////////////////////////
+window.addEventListener("load", function () {
+  const slowContent = document.querySelector(".form-2");
+  const loader = document.querySelector(".loader-2");
+
+  slowContent.style.visibility = "visible"; // Show the content
+  loader.style.display = "none"; // Hide the loader
+});
+
+// ///////////////////
+// ///////////////////
+document.addEventListener("DOMContentLoaded", function () {
+  const recordBtn = document.querySelector(".click");
+  const form1 = document.querySelector(".form-1");
+
+  recordBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    form1.style.display = "block";
+  });
+});
+
+///////////////////// handle videos section /////////////////////////
+// Select all thumbnail images
+const thumbnails = document.querySelectorAll(".video-thumbnail");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    const videoContainer = thumbnail.closest(".video-container");
+    const videoFrame = videoContainer.querySelector("iframe");
+
+    // Add the fade-out class to trigger the animation
+    thumbnail.classList.add("fade-out");
+
+    // Wait for the animation to finish before hiding the thumbnail
+    thumbnail.addEventListener("animationend", () => {
+      thumbnail.style.display = "none"; // Hide thumbnail
+      videoFrame.style.display = "block"; // Show video
+    });
+  });
 });
